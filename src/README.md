6245058f111ceedf7a188d73e18f3af59ec3f026
pikaqqqqq <281705236@qq.com>
2018-01-07 16:56:23 +0800

    Initial commit

---
+++ src/README.md
@@ -0,0 +1,3 @@
+# Submission Detail
+
+* 很多数组问题都可以转为排序问题来处理
---
+++ src/main.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+/**
+ * Created by think on 2018/1/7.
+ */
+/*
+* 测试数据：
+* 大数据、空数据、有重复、没有重复
+* */
+public class main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 8, 10};
+        System.out.println(containsDuplicate2(nums));
+    }
+
+    //大数据超时
+    public static boolean containsDuplicate1(int[] nums) {
+        if (nums == null || nums.length < 2) return false;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == nums[i] && i != j) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //排序法ok
+    public static boolean containsDuplicate2(int[] nums) {
+        if (nums == null || nums.length < 2) return false;
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
